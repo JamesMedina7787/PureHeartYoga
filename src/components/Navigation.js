7cee50adea0fc6e1e8e8f2f84222fc10f5b5cfcf
james medina <jamesmedina7787@outlook.com>
2021-09-30 14:37:17 -0400

    cell phone optimization

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,10 @@ class Navigation extends Component {
   render() {
     console.log()
     return (
-      <div className="foldOverContainer">
+      <div className="foldOverContainer" >
         <NavLink className="foldOver"  to="/FoldOverItem">About</NavLink><br/>
         <NavLink className="foldOver"  to="/FoldOverItem2">Links</NavLink> <br/>
-    
+
         <NavLink className="foldOver"  to="/FoldOverItem4">Blog</NavLink><br/>
       </div>
     );
